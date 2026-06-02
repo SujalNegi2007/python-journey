3452fefa7669b77bfd37c69398f44088b74e4faa
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-06-02 16:15:26 +0530

    Add utility functions for user interaction

---
+++ day15/utils.py
@@ -0,0 +1,21 @@
+def yes_no():
+    print("Are You Sure(yes/no)?")
+    while True:
+        user_input = input("Your Reply: ").capitalize().strip()
+        if user_input == "Yes":
+            print("...")
+            return "True"
+        elif user_input == "No":
+            print("Going Back...")
+            return "False"
+        else:
+            print("Only Yes And No Are Avaliable!")
+
+def window():
+    print("\n+---------------------------------------------+\n| To Borrow A Book                : Enter [1] |\n| To Check If A Book Is Avaliable : Enter [2] |\n| Want To Borrow A Random Book    : Enter [3] |\n| Want To Return A Book           : Enter [4] |\n| To Check Shelf                  : Enter [5] |\n| To Exit The Library             : Enter [6] |\n+---------------------------------------------+\n")
+
+def name():
+    user_name = input("\nEnter Your Name: ").capitalize().strip()
+    split_name = user_name.split(" ")
+    first_name = split_name[0]
+    return first_name
