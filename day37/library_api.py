f1da4b87d82da37821e41b0fa828963aafeda28e
SujalNegi2007 <sujalnegi0411@gmail.com>
2026-08-29 21:36:35 +0530

    Refactor API functions and update user prompts
    Refactor functions to improve clarity and error handling. Update user input prompts and change output file name.

--- day37/library_api.py
+++ day37/library_api.py
@@ -1,44 +1,46 @@
 import requests
-import json
 from pathlib import Path
+import json
 
-def fetch_package_data(package_name):
+def get_features(package_name):
     url = f'https://pypi.org/pypi/{package_name}/json'
-
-
-    response = requests.get(url, timeout = 10)
-
-    if response.status_code == 200:
-        data = response.json()
+    get_response = requests.get(url, timeout = 10)
+    if get_response.status_code == 200:
+        data = get_response.json()
         info = data['info']
         return {
             'Name' : info['name'],
             'Version' : info['version'],
             'Summary' : info['summary']
         }
-
     else:
-        print(f"Error Detected: {response.status_code}")
-        return None
+        print(f"Error: {get_response.status_code}")
 
-results = []
+def create_user(name, job):
+    url = 'https://reqres.in/api/users'
+    response = requests.post(url, json = {
+                'Name' : name,
+                'Job' : job
+            })
+    
+    if response.status_code not in [200, 201]:
+        print(f'Error: {response.status_code}')
+        return
 
-for pkg in ['numpy', 'django']:
-    result = fetch_package_data(pkg)
-    if result:
-        results.append(result)
+    return response.json()
 
-print(results)
+results = {
+    "get_result" : get_features('numpy'),
+    "post_result" : create_user('Sujal', 'MLOPS Enginner')
+}
 
 while True:
-    answer = input('Want To Save This To Json File(Y/N)?').upper().strip()
-    if answer and answer[0] in ['Y', 'N']:
-        if answer[0] == 'Y':
-            with Path('api_data.json').open('w', encoding = 'utf-8') as file:
-                json.dump(results, file, indent = 2)
-            print("File Saved.")
-        else:
-            print('File Not Saved')
+    answer = input('Do you want to save the file(Yes/No)?').upper().strip()
+    if answer and answer[0] in ('Y', 'Yes'):
+        with Path('combined.json').open('w', encoding = 'utf-8') as file:
+            json.dump(results, file, indent = 2)
+        print('File Saved')
         break
     else:
-        print("Only Yes And No Are Available.")
+        print('File Not Saved.')
+        break
